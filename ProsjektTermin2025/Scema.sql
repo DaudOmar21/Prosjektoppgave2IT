ad284d1a9f74c605f38b7b3d7dbb411f63f367aa
Daud <idil29@hotmail.com>
2025-12-12 11:53:07 +0100

    Delete ProsjektTermin2025 directory

--- ProsjektTermin2025/Scema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE products (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    description TEXT,
-    price REAL NOT NULL,
-    category TEXT,
-    stock INTEGER,
-    image_url TEXT
-);
